60fd53f96dee855a2533f13678f0a7608dd8484d
Gaurav Chaubey <luciferuiop906@gmail.com>
2021-12-26 13:34:23 +0530

    firebase_add

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyD1X8H6zVNwmBEObK3jXcyBuRAAvUh1LKU',
+    authDomain: 'book-heaven-c7e20.firebaseapp.com',
+    projectId: 'book-heaven-c7e20',
+    storageBucket: 'book-heaven-c7e20.appspot.com',
+    messagingSenderId: '228868901762',
+    appId: '1:228868901762:web:5d7918c7f89cd3aca71d9b',
+    measurementId: 'G-9FXQLMV1WG',
+}
